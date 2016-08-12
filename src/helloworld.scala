cc155b089f2cd4641332133d3b25bd2eeb724f7c
sferefchillipeppers <lk.huaman@gmail.com>
2016-08-12 00:48:21 -0700

    Inciando el curso de scala.

--- src/helloworld.scala
+++ src/helloworld.scala
@@ -2,11 +2,7 @@
   * Created by docker on 12/08/16.
   */
 object helloworld {
-
   def main(args: Array[String])={
-
     println("Esto es una prueba de que git está bien configurado.")
-
   }
-
 }
